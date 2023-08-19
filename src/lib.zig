050179fbd6c83872358e7de27d40dfc8345ded93
Felix "xq" Queißner <git@random-projects.net>
2023-08-19 17:38:53 +0200

    Adds support for freestanding cstd

--- src/lib.zig
+++ src/lib.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const modules = @import("modules");
 comptime {
     if (modules.glibcstart) _ = @import("glibcstart.zig");
@@ -6,3 +7,19 @@ comptime {
     if (modules.linux) _ = @import("linux.zig");
     if (modules.gnu) _ = @import("gnu.zig");
 }
+
+const builtin = @import("builtin");
+
+pub const std_options = if (builtin.target.os.tag == .freestanding) struct {
+    pub fn logFn(
+        comptime message_level: std.log.Level,
+        comptime scope: @TypeOf(.enum_literal),
+        comptime format: []const u8,
+        args: anytype,
+    ) void {
+        _ = args;
+        _ = format;
+        _ = scope;
+        _ = message_level; //
+    }
+} else struct {};
